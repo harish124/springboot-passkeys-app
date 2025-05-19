926480b7dea07eea85eec94ff768f61eaac51fd8
Harish <balajiharish4@gmail.com>
2025-05-19 16:22:18 +0800

    Initialized app

---
+++ src/test/java/com/pqs/passkeys/PasskeysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pqs.passkeys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasskeysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
